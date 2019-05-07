7cc98df8a45022c87e0af9dd0351239d45f1cbe5
Yosuke Onoue <onoue@likr-lab.com>
2019-05-07 22:04:58 +0900

    beginners selection

---
+++ AtCoder Beginners Selection/ABC086C.rs
@@ -0,0 +1,80 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+
+fn check() {
+    input!{
+        n: usize,
+        rows: [(i32, i32, i32); n],
+    }
+    let mut cx = 0;
+    let mut cy = 0;
+    let mut t0 = 0;
+    for row in rows {
+        let (t, x, y) = row;
+        let d = (x - cx).abs() + (y - cy).abs();
+        if d > t - t0 || (t - t0) % 2 != d % 2 {
+            println!("NO");
+            return;
+        }
+        cx = x;
+        cy = y;
+        t0 = t;
+    }
+    println!("YES");
+}
+
+fn main() {
+    check();
+}
+
