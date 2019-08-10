5cbb6bcdc05c55cd180284d46d96042f6cbc2726
Yosuke Onoue <onoue@likr-lab.com>
2019-08-10 22:43:49 +0900

    abc137

---
+++ 20190810abc137/D.rs
@@ -0,0 +1,101 @@
+#[allow(unused_macros)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+#[allow(unused_macros)]
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, [ $t:tt ]) => {
+        {
+            let len = read_value!($next, usize);
+            (0..len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+        }
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, bytes) => {
+        read_value!($next, String).into_bytes()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        ab: [(usize, usize); n],
+    }
+    let mut ab = ab;
+    ab.sort_by_key(|row| row.0);
+
+    let mut v = Vec::with_capacity(n);
+    let mut index = 0;
+    let mut total = 0;
+    for remains in 1..m + 1 {
+        let last_index = 0;
+        while index < n && ab[index].0 <= remains {
+            let b = ab[index].1;
+            let j = match v.binary_search(&b) {
+                Ok(j) => j,
+                Err(j) => j,
+            };
+            v.insert(j, b);
+            index += 1;
+        }
+        if let Some(b) = v.pop() {
+            total += b;
+        }
+    }
+    println!("{}", total);
+}
