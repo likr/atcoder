c37b5bc882c8e8ae4a65a57c26dee05137a1f2da
Yosuke Onoue <onoue@likr-lab.com>
2022-01-04 21:14:38 +0900

    typical90

--- typical90/src/bin/067.rs
+++ typical90/src/bin/067.rs
@@ -4,7 +4,8 @@ use proconio::marker::*;
 #[allow(unused_imports)]
 use std::cmp::*;
 #[allow(unused_imports)]
-use std::collections::*; #[allow(unused_imports)]
+use std::collections::*;
+#[allow(unused_imports)]
 use std::f64::consts::*;
 
 #[allow(unused)]
@@ -20,9 +21,41 @@ macro_rules! debug {
     };
 }
 
+fn to_digits(mut x: usize, digits: &mut Vec<usize>, base: usize) {
+    digits.clear();
+    while x > 0 {
+        digits.push(x % base);
+        x /= base;
+    }
+}
+
+fn from_digits(digits: &Vec<usize>, base: usize) -> usize {
+    let mut x = 0;
+    let mut b = 1;
+    for &d in digits.iter() {
+        x += d * b;
+        b *= base;
+    }
+    x
+}
+
 fn main() {
     input! {
         n: usize,
+        k: usize,
     }
-    println!("{}", n);
+    let mut digits = vec![];
+    to_digits(n, &mut digits, 10);
+    let mut result = from_digits(&digits, 8);
+    for _ in 0..k {
+        to_digits(result, &mut digits, 9);
+        for i in 0..digits.len() {
+            if digits[i] == 8 {
+                digits[i] = 5;
+            }
+        }
+        result = from_digits(&digits, 8)
+    }
+    to_digits(result, &mut digits, 8);
+    println!("{}", from_digits(&digits, 10));
 }
