25e2fa940e21e4b4c72658ee804aa8423d6e1d96
Yosuke Onoue <onoue@likr-lab.com>
2024-05-19 11:29:44 +0900

    update

--- abc354/src/bin/f.rs
+++ abc354/src/bin/f.rs
@@ -7,9 +7,10 @@ use std::cmp::*;
 use std::collections::*;
 #[allow(unused_imports)]
 use std::f64::consts::*;
+use superslice::*;
 
 #[allow(unused)]
-const INF: usize = std::usize::MAX / 4;
+const INF: i64 = std::i64::MAX / 4;
 #[allow(unused)]
 const M: usize = 1000000007;
 
@@ -21,9 +22,49 @@ macro_rules! debug {
     };
 }
 
+fn lis(a: &Vec<i64>) -> Vec<usize> {
+    let n = a.len();
+    let mut dp = vec![INF; n];
+    let mut l = vec![0; n];
+    for i in 0..n {
+        let k = dp.lower_bound(&a[i]);
+        dp[k] = a[i];
+        l[i] = k + 1;
+    }
+    l
+}
+
 fn main() {
     input! {
-        n: usize,
+        t: usize,
     }
-    println!("{}", n);
+    for _ in 0..t {
+        input! {
+            n: usize,
+            mut a: [i64; n],
+        }
+        let lis_left = lis(&a);
+        a.reverse();
+        for i in 0..n {
+            a[i] = -a[i];
+        }
+        let mut lis_right = lis(&a);
+        a.reverse();
+        for i in 0..n {
+            a[i] = -a[i];
+        }
+        lis_right.reverse();
+        debug!(lis_left);
+        debug!(lis_right);
+
+        let m = lis_left.iter().max().unwrap();
+        let mut ans = vec![];
+        for i in 0..n {
+            if lis_left[i] + lis_right[i] == m + 1 {
+                ans.push(format!("{}", i + 1));
+            }
+        }
+        println!("{}", ans.len());
+        println!("{}", ans.join(" "));
+    }
 }
